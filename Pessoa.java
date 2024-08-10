11a82f83dbe80c62aab3d3858bc4cad20d15f482
Camila <87709951+Latamila@users.noreply.github.com>
2024-08-10 13:27:40 -0300

    Create Pessoa.java
    classe Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,23 @@
+
+public class Pessoa {
+	int idade;
+	int qtdcriancas;
+	int idadeCriancaMaisNova;
+	boolean deficiencia;
+	
+	
+	public void pedeSenha() {
+		System.out.println("Pegue uma senha. ");
+	}
+	
+	public void sentar() {
+		System.out.println("Sente-se e aguarde. ");
+		
+	}
+	
+	public void serAtendido() {
+		System.out.println("Siga para o caixa para ser atendido.");
+	}
+	
+}
+
