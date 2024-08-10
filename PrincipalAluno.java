334bcf08763a541b14d2dc827d7dda2d1c86b056
Camila <87709951+Latamila@users.noreply.github.com>
2024-08-10 18:00:37 -0300

    Create PrincipalAluno.java

---
+++ PrincipalAluno.java
@@ -0,0 +1,18 @@
+
+public class PrincipalAluno {
+
+	public static void main(String[] args) {
+		Aluno Camila = new Aluno();
+		Camila.bim1 = 70;
+		Camila.bim2 = 60;
+		Camila.bim3 = 80;
+		Camila.bim4 = 70;
+		
+		
+		System.out.println(Camila.media());
+		System.out.println(Camila.passouDeAno());
+		// TODO Auto-generated method stub
+
+	}
+
+}
