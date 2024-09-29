a85868da44eb1a704165de0591294af15d35294a
Camila <87709951+Latamila@users.noreply.github.com>
2024-09-28 21:07:51 -0300

    Create condicionalRepeticao.java

---
+++ condicionalRepeticao.java
@@ -0,0 +1,23 @@
+
+public class condicionalRepeticao {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		while (count < 10) {
+			System.out.println(++count);
+		}
+		
+		int conta = 0;
+		do {
+			System.out.println("dentro do do-while= " + ++conta);
+			
+		} while (conta < 10);
+		
+		int contagem = 0;
+		for (int i=0; i < 10; i++) {
+			System.out.println("For " + i);
+		}
+		// TODO Auto-generated method stub
+	}
+}
