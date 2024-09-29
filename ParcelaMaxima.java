797d577bfa2b9bdf71199a2d28bf77c7e15cb6eb
Camila <87709951+Latamila@users.noreply.github.com>
2024-09-28 21:45:21 -0300

    Create ParcelaMaxima.java

---
+++ ParcelaMaxima.java
@@ -0,0 +1,15 @@
+
+public class ParcelaMaxima {
+
+	public static void main(String[] args) {
+		double valorTotal = 30000;
+		for (int parcela = 1; parcela <= valorTotal; parcela++) {
+			double valorParcela = valorTotal / parcela;
+			if( valorParcela < 1000) {
+				break;
+			}
+			System.out.println("Parcela de " + parcela + " vezes= " + "R$ " + valorParcela);
+		}
+		
+	}
+}
